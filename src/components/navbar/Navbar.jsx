09dfb2ece54c01161a5546e3740c2303733d8c61
HAMZA PALA <hamzapala48@gmail.com>
2022-04-06 03:24:59 +0300

    ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-md navbar-dark bg-dark">
         <div className="container">
           <Link to="/"  className="navbar-brand">
-            recap/şaban/ramazan
+            Recep/Shaban/Ramazan
           </Link>
 
           <div>
